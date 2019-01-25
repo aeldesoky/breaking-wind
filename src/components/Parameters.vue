f90d17425a2a8a6acaa4961237a4ec87abba9f57
Brandon Richardson <brandon1024.br@gmail.com>
2019-01-25 17:28:24 -0400

    Fixed params cards

--- src/components/Parameters.vue
+++ src/components/Parameters.vue
@@ -7,136 +7,137 @@ analysis.
 
 <template>
   <v-card class="parameters">
-    <div>
-      <span class="headline mb-0">General Options</span>
-      <button
-          type="button"
-          class="btn"
-          @click="showModal"
+    <v-card class="card" style="margin-top: 0">
+      <div>
+        <span class="headline mb-0">General Options</span>
+        <button
+            type="button"
+            class="btn"
+            @click="showModal"
+        >
+          <v-icon right>help</v-icon>
+        </button>
+      </div>
+
+      <v-form v-model="valid">
+        <v-slider
+          :value="general.budget / 1000000"
+          @input="setOption('budget', $event * 1000000)"
+          max="100"
+          label="Budget (Million $)"
+          thumb-label
+        ></v-slider>
+
+        <v-slider
+          :value="general.discountRate"
+          @input="setOption('discountRate', $event)"
+          max="20"
+          ticks
+          label="Discount Rate (%)"
+          thumb-label
+        ></v-slider>
+
+        <v-slider
+          :value="general.outputDecreasePerYear"
+          @input="setOption('outputDecreasePerYear', $event)"
+          max="20"
+          ticks
+          label="Output Decrease (%)"
+          thumb-label
+        ></v-slider>
+
+        <v-slider
+          :value="general.lifeSpan"
+          @input="setOption('lifeSpan', $event)"
+          max="40"
+          min="10"
+          ticks
+          label="Lifespan (Years)"
+          thumb-label
+        ></v-slider>
+      </v-form>
+    </v-card>
+
+    <v-card class="card">
+      <div style="display: flex">
+        <h3 class="headline mb-0 turbine-title">
+          Turbine
+        </h3>
+        <v-spacer></v-spacer>
+        <v-select
+          class="turbine-select"
+          :items="turbineOptions"
+          label="Turbine"
+          :value="data.turbine"
+          @input="data.selectTurbine"
+        ></v-select>
+        <!-- <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div> -->
+      </div>
+
+      <v-form
+        v-model="valid"
+        v-if="turbine"
       >
-        <v-icon right>help</v-icon>
-      </button>
-    </div>
+        <v-slider
+          :disabled="turbine.disabled"
+          :value="turbine.nominalPowerAt"
+          @input="changeTurbine('nominalPowerAt', $event)"
+          max="30"
+          label="Nominal Power At (m/s)"
+          thumb-label
+        ></v-slider>
 
-    <v-form v-model="valid">
-      <v-slider
-        :value="general.budget / 1000000"
-        @input="setOption('budget', $event * 1000000)"
-        max="100"
-        label="Budget (Million $)"
-        thumb-label
-      ></v-slider>
+        <v-slider
+          :disabled="turbine.disabled"
+          :value="turbine.nominalPower / 1000"
+          @input="changeTurbine('nominalPower', $event * 1000)"
+          max="10"
+          label="Nominal Power (MW)"
+          thumb-label
+        ></v-slider>
 
-      <v-slider
-        :value="general.discountRate"
-        @input="setOption('discountRate', $event)"
-        max="20"
-        ticks
-        label="Discount Rate (%)"
-        thumb-label
-      ></v-slider>
+        <v-slider
+          :disabled="turbine.disabled"
+          :value="turbine.unitCost / 1000000"
+          @input="changeTurbine('unitCost', $event * 1000000)"
+          max="50"
+          ticks
+          label="Unit Cost (Million)"
+          thumb-label
+        ></v-slider>
 
-      <v-slider
-        :value="general.outputDecreasePerYear"
-        @input="setOption('outputDecreasePerYear', $event)"
-        max="20"
-        ticks
-        label="Output Decrease (%)"
-        thumb-label
-      ></v-slider>
+        <v-slider
+          :disabled="turbine.disabled"
+          :value="turbine.maintenance / 1000000"
+          @input="changeTurbine('maintenance', $event * 1000000)"
+          max="2"
+          ticks
+          step="0.1"
+          label="Maintenance (Million/year)"
+          thumb-label
+        ></v-slider>
 
-      <v-slider
-        :value="general.lifeSpan"
-        @input="setOption('lifeSpan', $event)"
-        max="40"
-        min="10"
-        ticks
-        label="Lifespan (Years)"
-        thumb-label
-      ></v-slider>
-    </v-form>
+        <v-slider
+          :disabled="turbine.disabled"
+          :value="turbine.timeToConstruct"
+          @input="changeTurbine('timeToConstruct', $event)"
+          max="5"
+          ticks
+          label="Time To Construct (Years)"
+          thumb-label
+        ></v-slider>
 
-    <v-divider></v-divider>
-    
-
-    <div style="display: flex">
-      <h3 class="headline mb-0 turbine-title">
-        Turbine
-      </h3>
-      <v-spacer></v-spacer>
-      <v-select
-        class="turbine-select"
-        :items="turbineOptions"
-        label="Turbine"
-        :value="data.turbine"
-        @input="data.selectTurbine"
-      ></v-select>
-      <!-- <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div> -->
-    </div>
-
-    <v-form 
-      v-model="valid"
-      v-if="turbine"
-    >
-      <v-slider
-        :disabled="turbine.disabled"
-        :value="turbine.nominalPowerAt"
-        @input="changeTurbine('nominalPowerAt', $event)"
-        max="30"
-        label="Nominal Power At (m/s)"
-        thumb-label
-      ></v-slider>
-
-      <v-slider
-        :disabled="turbine.disabled"
-        :value="turbine.nominalPower / 1000"
-        @input="changeTurbine('nominalPower', $event * 1000)"
-        max="10"
-        label="Nominal Power (MW)"
-        thumb-label
-      ></v-slider>
-
-      <v-slider
-        :disabled="turbine.disabled"
-        :value="turbine.unitCost / 1000000"
-        @input="changeTurbine('unitCost', $event * 1000000)"
-        max="50"
-        ticks
-        label="Unit Cost (Million)"
-        thumb-label
-      ></v-slider>
-
-      <v-slider
-        :disabled="turbine.disabled"
-        :value="turbine.maintenance / 1000000"
-        @input="changeTurbine('maintenance', $event * 1000000)"
-        max="2"
-        ticks
-        step="0.1"
-        label="Maintenance (Million/year)"
-        thumb-label
-      ></v-slider>
-
-      <v-slider
-        :disabled="turbine.disabled"
-        :value="turbine.timeToConstruct"
-        @input="changeTurbine('timeToConstruct', $event)"
-        max="5"
-        ticks
-        label="Time To Construct (Years)"
-        thumb-label
-      ></v-slider>
-
-      <v-checkbox
-        label="Disabled"
-        :value="turbine.disabled"
-        @change="changeTurbine('disabled', $event)"
-      ></v-checkbox>
-    </v-form>
+        <v-checkbox
+          label="Disabled"
+          :value="turbine.disabled"
+          @change="changeTurbine('disabled', $event)"
+        ></v-checkbox>
+      </v-form>
+    </v-card>
 
     <v-expansion-panel
       v-model="panel"
-      expand
+      expand class="optional-costs-card"
     >
       <v-expansion-panel-content class="optional-costs">
         <div class = "optional-title" slot="header">
@@ -315,7 +316,15 @@ export default class Parameters extends Vue {
   margin-top: 15px
   margin-bottom: 15px
 
+.card
+  padding: 15px
+  margin-top: 15px
+  margin-bottom: 15px
+
 .optional-costs
   padding-left: 15px
   padding-right: 15px
+
+.optional-costs-card
+  margin-bottom: 15px
 </style>
